c76fff53440300bce923c904f5cf55856418bbd2
Jongui <joaogd53@gmail.com>
2017-10-18 20:23:08 -0200

    Added description to class

--- classes.py
+++ classes.py
@@ -2,13 +2,14 @@ import MySQLdb
 import json
 
 class Classes:
-    def __init__(self, idCourses, idClasses, date):
+    def __init__(self, idCourses, idClasses, date, description):
         self.idCourses = idCourses
         self.idClasses = idClasses
         self.date = date
+        self.description = description
 
     def to_json(self):
-        ret = json.dumps({"idCourses":self.idCourses, "idClasses": self.idClasses, "date":self.date.strftime('%Y-%m-%d')})
+        ret = json.dumps({"idCourses":self.idCourses, "idClasses": self.idClasses, "date":self.date.strftime('%Y-%m-%d'), "description":self.description})
         return ret
 
 class ClassesDAO:
@@ -18,7 +19,7 @@ class ClassesDAO:
         curs = db.cursor()
         curs.execute("SELECT * FROM Classes WHERE idCourses=%s", id_cours,)
         for reading in curs.fetchall():
-            classes = Classes(reading[0], reading[1], reading[2])
+            classes = Classes(reading[0], reading[1], reading[2], reading[3])
             output += classes.to_json() + ","
         db.close();
         output = output[:-1]
